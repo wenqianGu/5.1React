68ea4b16d3955698e05324784be2e8305475cd3e
Lisa_GU <33195278+wenqianGu@users.noreply.github.com>
2022-08-16 15:12:55 +1000

    Update Header.js

--- React1.0/app/Header/Header.js
+++ React1.0/app/Header/Header.js
@@ -1,34 +1,66 @@
 import React from "react";
-import './Header.css';
-// import styled from 'styled-components';
-// import Flex from '../../components/Flex/Flex';
+// import './Header.css';
+import styled from 'styled-components';
+import Flex from '../../components/Flex/Flex';
 // 索引文件的时候，找到的是NavbarItem里面的index
 // import NavbarItem, {Link} from "./components/NavbarItem";
 
-// const Highlight = styled.span`
-//   color: #2eca7f;
-// `;
-// const Logo = styled.div`
-//   font-size: 1.5rem;
-//   font-weight: 500;
-// `;
+const Highlight = styled.span`
+  color: #2eca7f;
+`;
+const Logo = styled.div`
+  font-size: 1.5rem;
+  font-weight: 500;
+`;
 // // Nav是一个Flex，但是是一个添加了很多样式的Flex
-// const Nav = styled(Flex)`
-//   padding: 15px 0;
-//   align-items: center;
-// `;
-//
-// const Left = styled.div`
-//   flex: 1;
-// `;
-// // function take string arguments, 省略（`flex:1`）-> div就是一个function
-// const Right = styled.div`
-//
-// `;
-// // const Navbar = styled.div`
-// //   display: flex;
-// // `;
-//
+const Nav = styled(Flex)`
+  padding: 15px 0;
+  align-items: center;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+// function take string arguments, 省略（`flex:1`）-> div就是一个function
+const Right = styled.div`
+
+`;
+const Navbar = styled.div`
+  display: flex;
+`;
+
+const NavbarItem = styled.button`
+    border:0;
+    background: transparent;
+    cursor: pointer;
+    padding: 16px;
+    text-decoration: none;
+    color: #49515d;
+    font-size: 15px;
+    opacity: ${(props) => props.active ? '1' : '0.6'}; 
+    display: block;
+    transition: opacity 0.3s ease-in-out;
+  
+    &::after {
+        content: "";
+        width: ${({active}) => active ? '24px' : '0'}; 
+        border-bottom: 3px solid #377e9a;
+        margin: auto;
+        margin-top: 4px;
+        display: block;
+        transition: width 0.3s ease-in-out;
+    }
+
+    &:hover {
+        opacity: 1;
+    }
+
+
+    &:hover::after {
+        width: 24px;
+    }
+`;
+
 // // Readable Maintainable
 // // Reusable
 /**
@@ -36,44 +68,72 @@ import './Header.css';
  * HTML 里面给的是“” 只要是attribute的值，永远都是双引号“”
  * JS里面是“”(市场占用率20%多) 或者 '' （市场占有率70%） ->根据项目要求，要统一；
  * */
-const Header = (props) => {
-    const {page} = props;
+// function component -> 用花括号的形式去接受参数；
+const Header = ({
+    page,
+    onPageChnage,
+}) => {
+    //const { page } = props;
     // const page = "SERVICES";
     const navbarItems = [
-        {key: 'HOME', value: 'Home'},
-        {key: 'RESUME', value: 'Resume'},
-        {key: 'SERVICES', value: 'Services'},
-        {key: 'BLOG', value: 'Blog'},
-        {key: 'CONTACT', value: 'Contact'}
+        { key: 'HOME', value: 'Home' },
+        { key: 'RESUME', value: 'Resume' },
+        { key: 'SERVICES', value: 'Services' },
+        { key: 'BLOG', value: 'Blog' },
+        { key: 'CONTACT', value: 'Contact' }
     ];
-    const getNavbarItemActiveClassName = (key) => page === key && 'navbar__item--active';
-    const getNavbarItemClassName = (key) => `navbar__item ${getNavbarItemActiveClassName(key)}`;
+    // const getNavbarItemActiveClassName = (key) => page === key && 'navbar__item--active';
+    // const getNavbarItemClassName = (key) => `navbar__item ${getNavbarItemActiveClassName(key)}`;
     // const getNavbarItemClassName = (key) => `navbar__item ${page === key && 'navbar__item--active'}`;
     return (
-        <header className="nav">
-            <div className="nav__left">
-                <div className="logo">
-                    <span className="logo__highlight">Lisa</span>
+        <Nav>
+            <Left>
+                <Logo className="logo">
+                    <Highlight>Lisa</Highlight>
                     GU
-                </div>
-            </div>
-            <div className="nav__right">
-                <div className="navbar">
-                    { navbarItems.map((navbarItem) => (
-                      <a
-                          key ={navbarItem.key}
-                          className={getNavbarItemClassName(navbarItem.key)} href={navbarItem.value}>
-                          {navbarItem.value}
-                      </a>
+                </Logo>
+            </Left>
+            <Right>
+                <Flex>
+                    {navbarItems.map((navbarItem) => (
+                        <NavbarItem
+                            onClick = { () => {
+                                onPageChnage(navbarItem.key);
+                            }}
+                            key = {navbarItem.key}
+                            active={page === navbarItem.key}
+                            href={navbarItem.value}
+                        >
+                            {navbarItem.value}
+                        </NavbarItem>
                     ))}
-                    {/*<a className={getNavbarItemClassName('HOME')} href="HOME">Home</a>*/}
-                    {/*<a className={getNavbarItemClassName('RESUME')} href="RESUME">Resume</a>*/}
-                    {/*<a className={getNavbarItemClassName('SERVICES')} href="SERVICES">Services</a>*/}
-                    {/*<a className={getNavbarItemClassName('BLOG')} href="BLOG">Blog</a>*/}
-                    {/*<a className={getNavbarItemClassName('CONTACT')} href="CONTACT">Contact</a>*/}
-                </div>
-            </div>
-        </header>
+                </Flex>
+            </Right>
+        </Nav>
+        // <header className="nav">
+        //     <div className="nav__left">
+        //         <div className="logo">
+        //             <span className="logo__highlight">Lisa</span>
+        //             GU
+        //         </div>
+        //     </div>
+        //     <div className="nav__right">
+        //         <div className="navbar">
+        //             { navbarItems.map((navbarItem) => (
+        //               <a
+        //                   key ={navbarItem.key}
+        //                   className={getNavbarItemClassName(navbarItem.key)} href={navbarItem.value}>
+        //                   {navbarItem.value}
+        //               </a>
+        //             ))}
+        //             {/*<a className={getNavbarItemClassName('HOME')} href="HOME">Home</a>*/}
+        //             {/*<a className={getNavbarItemClassName('RESUME')} href="RESUME">Resume</a>*/}
+        //             {/*<a className={getNavbarItemClassName('SERVICES')} href="SERVICES">Services</a>*/}
+        //             {/*<a className={getNavbarItemClassName('BLOG')} href="BLOG">Blog</a>*/}
+        //             {/*<a className={getNavbarItemClassName('CONTACT')} href="CONTACT">Contact</a>*/}
+        //         </div>
+        //     </div>
+        // </header>
     );
 }
 
